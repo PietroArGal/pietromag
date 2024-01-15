228e66ee3fb6dcf33f477ef9611c7c96a857476d
pietroargal <pietromauricioag@outlook.com>
2024-01-14 23:42:13 -0500

    last updt

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -39,6 +39,7 @@ import {
     User,
     PaintBucket,
     ScrollText,
+    ArrowRightCircle,
 } from "lucide-react";
 import Link from "next/link";
 
@@ -112,7 +113,9 @@ export default function ServicesPage() {
                                 </div>
 
                                 <DialogTrigger>
-                                    <Switch />
+                                    <Button className="hover:bg-neutral-300">
+                                        <ArrowUpRight />
+                                    </Button>
                                 </DialogTrigger>
                                 <DialogContent>
                                     <DialogHeader>
@@ -138,7 +141,9 @@ export default function ServicesPage() {
                                     </p>
                                 </div>
                                 <DialogTrigger>
-                                    <Switch />
+                                    <Button className="hover:bg-neutral-300">
+                                        <ArrowUpRight />
+                                    </Button>
                                 </DialogTrigger>
                                 <DialogContent>
                                     <DialogHeader>
@@ -163,7 +168,9 @@ export default function ServicesPage() {
                                     </p>
                                 </div>
                                 <DialogTrigger>
-                                    <Switch />
+                                    <Button className="hover:bg-neutral-300">
+                                        <ArrowUpRight />
+                                    </Button>
                                 </DialogTrigger>
                                 <DialogContent>
                                     <DialogHeader>
@@ -186,128 +193,128 @@ export default function ServicesPage() {
                                 </div>
                             </div>
                         </div>
+                        <Dialog>
+                            <DialogTrigger>
+                                <Button className="w-full">
+                                    <User className="mr-2 h-5 w-5" /> Contacto Rápido
+                                </Button>
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle>Contacto</DialogTitle>
+                                    <div className="py-1"></div>
+                                    <DialogDescription className="flex flex-col gap-3">
+                                        <Link
+                                            href="tel:+51975677451"
+                                            className="flex gap-1 justify-center"
+                                        >
+                                            <Phone strokeWidth={1} size={20} />
+                                            <p className="text-sm">+51 975 677 451</p>
+                                        </Link>
+                                        <Link
+                                            href="mailto:pietromauag@gmail.com"
+                                            className="flex gap-1 justify-center"
+                                        >
+                                            <Mail strokeWidth={1} size={20} />
+                                            <p className="text-sm">pietromauag@gmail.com</p>
+                                        </Link>
+                                        <Link
+                                            href=""
+                                            className="flex gap-1 justify-center cursor-default"
+                                        >
+                                            <Map strokeWidth={1} size={20} />
+                                            <p className="text-sm">Lima, Perú - Miraflores</p>
+                                        </Link>
+                                    </DialogDescription>
+
+                                    <div className="py-2">
+                                        <br />
+                                    </div>
+
+                                    <DialogTitle>Cotizaciones - Citas</DialogTitle>
+                                    <div className="py-1"></div>
+                                    <DialogDescription className="flex justify-center gap-6">
+                                        <Link
+                                            href="https://wa.me/51975677451"
+                                            target="_blank"
+                                            className="flex flex-col justify-center"
+                                        >
+                                            <Button className="w-50">
+                                                <Phone className="mr-2 h-4 w-4" />
+                                                Chat Directo
+                                            </Button>
+                                            <p className="mt-1">Whatsapp</p>
+                                        </Link>
+
+                                        <Link
+                                            href="https://calendly.com/pietromag/preguntas-frecuentes"
+                                            target="_blank"
+                                            className="flex flex-col justify-center"
+                                        >
+                                            <Button className="w-50">
+                                                <CalendarDays className="mr-2 h-4 w-4" />
+                                                Agendar Cita
+                                            </Button>
+                                            <p className="mt-1">Calendly</p>
+                                        </Link>
+                                    </DialogDescription>
+
+                                    <div className="py-2">
+                                        <br />
+                                    </div>
+
+                                    <DialogTitle>Curriculum - CV</DialogTitle>
+                                    <div></div>
+                                    <DialogDescription>
+                                        <Link
+                                            href="https://media.licdn.com/dms/document/media/D4E2DAQGeEa2BnqDKUQ/profile-treasury-document-pdf-analyzed/0/1686158576471?e=1697068800&v=beta&t=Yb4XJTMvLAqr4nRBiFKe93Wno55ilUbmO8m7RIflg5M"
+                                            target="_blank"
+                                        >
+                                            <Button variant="outline" className="w-50">
+                                                <FileText className="mr-2 h-4 w-4" />
+                                                Ver / Descargar
+                                            </Button>
+                                        </Link>
+                                    </DialogDescription>
+
+                                    <div>
+                                        <br />
+                                    </div>
+
+                                    <div></div>
+                                    <DialogDescription className="flex gap-3 justify-center">
+                                        <Link
+                                            href="https://www.linkedin.com/in/pietromag/"
+                                            target="_blank"
+                                        >
+                                            <Linkedin
+                                                strokeWidth={1}
+                                                size={24}
+                                                className="hover:fill-white"
+                                            />
+                                        </Link>
+                                        <Link href="https://github.com/PietroArGal" target="_blank">
+                                            <Github
+                                                strokeWidth={1}
+                                                size={24}
+                                                className="hover:fill-white"
+                                            />
+                                        </Link>
+                                    </DialogDescription>
+                                </DialogHeader>
+                            </DialogContent>
+                        </Dialog>
                     </CardContent>
-                    <CardFooter>
-                        <Button disabled className="w-full">
-                            <Loader2 className="mr-2 h-5 w-5 animate-spin" />
-                            Próximamente Web del Studio
-                        </Button>
-                    </CardFooter>
+
                 </Card>
             </div>
 
             <footer className="fixed bottom-6 w-full flex justify-center gap-x-5">
-                <Dialog>
-                    <DialogTrigger>
-                        <Button className="w-50">
-                            <User className="mr-2 h-5 w-5" /> Contácto sin compromiso
-                        </Button>
-                    </DialogTrigger>
-                    <DialogContent>
-                        <DialogHeader>
-                            <DialogTitle>Contacto</DialogTitle>
-                            <div className="py-1"></div>
-                            <DialogDescription className="flex flex-col gap-3">
-                                <Link
-                                    href="tel:+51975677451"
-                                    className="flex gap-1 justify-center"
-                                >
-                                    <Phone strokeWidth={1} size={20} />
-                                    <p className="text-sm">+51 975 677 451</p>
-                                </Link>
-                                <Link
-                                    href="mailto:pietromauag@gmail.com"
-                                    className="flex gap-1 justify-center"
-                                >
-                                    <Mail strokeWidth={1} size={20} />
-                                    <p className="text-sm">pietromauag@gmail.com</p>
-                                </Link>
-                                <Link
-                                    href=""
-                                    className="flex gap-1 justify-center cursor-default"
-                                >
-                                    <Map strokeWidth={1} size={20} />
-                                    <p className="text-sm">Lima, Perú - Miraflores</p>
-                                </Link>
-                            </DialogDescription>
+                <Button disabled>
+                    <Loader2 className="mr-2 h-5 w-5 animate-spin" />
+                    Próximamente Web del Studio
+                </Button>
 
-                            <div className="py-2">
-                                <br />
-                            </div>
-
-                            <DialogTitle>Cotizaciones - Citas</DialogTitle>
-                            <div className="py-1"></div>
-                            <DialogDescription className="flex justify-center gap-6">
-                                <Link
-                                    href="https://wa.me/51975677451"
-                                    target="_blank"
-                                    className="flex flex-col justify-center"
-                                >
-                                    <Button className="w-50">
-                                        <Phone className="mr-2 h-4 w-4" />
-                                        Chat Directo
-                                    </Button>
-                                    <p className="mt-1">Whatsapp</p>
-                                </Link>
-
-                                <Link
-                                    href="https://calendly.com/pietromag/preguntas-frecuentes"
-                                    target="_blank"
-                                    className="flex flex-col justify-center"
-                                >
-                                    <Button className="w-50">
-                                        <CalendarDays className="mr-2 h-4 w-4" />
-                                        Agendar Cita
-                                    </Button>
-                                    <p className="mt-1">Calendly</p>
-                                </Link>
-                            </DialogDescription>
-
-                            <div className="py-2">
-                                <br />
-                            </div>
-
-                            <DialogTitle>Curriculum - CV</DialogTitle>
-                            <div></div>
-                            <DialogDescription>
-                                <Link
-                                    href="https://media.licdn.com/dms/document/media/D4E2DAQGeEa2BnqDKUQ/profile-treasury-document-pdf-analyzed/0/1686158576471?e=1697068800&v=beta&t=Yb4XJTMvLAqr4nRBiFKe93Wno55ilUbmO8m7RIflg5M"
-                                    target="_blank"
-                                >
-                                    <Button variant="outline" className="w-50">
-                                        <FileText className="mr-2 h-4 w-4" />
-                                        Ver / Descargar
-                                    </Button>
-                                </Link>
-                            </DialogDescription>
-
-                            <div>
-                                <br />
-                            </div>
-
-                            <div></div>
-                            <DialogDescription className="flex gap-3 justify-center">
-                                <Link
-                                    href="https://www.linkedin.com/in/pietromag/"
-                                    target="_blank"
-                                >
-                                    <Linkedin
-                                        strokeWidth={1}
-                                        size={24}
-                                        className="hover:fill-white"
-                                    />
-                                </Link>
-                                <Link href="https://github.com/PietroArGal" target="_blank">
-                                    <Github
-                                        strokeWidth={1}
-                                        size={24}
-                                        className="hover:fill-white"
-                                    />
-                                </Link>
-                            </DialogDescription>
-                        </DialogHeader>
-                    </DialogContent>
-                </Dialog>
             </footer>
         </motion.div>
     );
